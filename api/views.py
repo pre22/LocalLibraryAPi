f8c02b05b652fcc6395084422cce92dd72ea87cc
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-17 12:03:21 +0100

    100% done

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from books.models import Book
+from api.serializers import BookSerializer
+
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
